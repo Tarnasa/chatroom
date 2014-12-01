929d963afad195f37400948db3ac646c5f2673b8
Joshua Wyss <t96rocks@gmail.com>
2014-12-01 10:50:34 -0600

    Client: setup socket and username

--- client.py
+++ client.py
@@ -1,28 +1,28 @@
-<<<<<<< Updated upstream
-import socket
-
-custom_port = 49152 #ports from 49152-65535 can be used for custom purposes
-bad_port = True
-chatroom = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-hostname = socket.gethostbyaddr(socket.gethostname())[0]
-
-chatroom.connect((hostname, custom_port))
-chatroom.send("hello, world")
-
-=======
 import sys
 import socket
 
+bad_port = True
+bad_user_name = True
 
-serverName = raw_input('What server do you want to connect to? ex. rc02xcs213.managed.mst.edu ')
-portNumber = raw_input('What port? ')
-userName = raw_input('What do you want your name to be? ex. Fred ')
+# Setting up connection
+while(bad_port)
+    try:
+        serverName = raw_input('What server do you want to connect to? ex. rc02xcs213.managed.mst.edu ')
+        portNumber = raw_input('What port? ')
+        sock = socket.create_connection((str(serverName), int(portNumber)))
+    except socket.error , msg:
+        print 'Binding failed. Error code: ' + str(msg[0]) + 'Error message: ' + msg[1]
+    else:
+        bad_port = False
+        print 'You successfully connected to ' + serverName + ' on port ' + portNumber + '\n'
 
-server_address = (str(serverName), int(portNumber))
-try:
-    socket.create_connection(server_address)
-else:
-    print('That did not work, try again. \n')
-    serverName = raw_input('What server do you want to connect to? ex. rc02xcs213.managed.mst.edu ')
-    portNumber = raw_input('What port? ')
->>>>>>> Stashed changes
+# Setting up user name
+while(bad_user_name):
+    try:
+        userName = raw_input('What do you want your name to be? ex. Fred ')
+        sock.sendall(userName)
+    except socket.recv(), e:
+        print 'Server said that ' + userName + ' is already in use. Try another one.\n'
+    else:
+        bad_user_name = False
+
--- connection_tester.py
+++ connection_tester.py
@@ -1,10 +1,10 @@
 import socket
 
-custom_port = 49152 #ports from 49152-65535 can be used for custom purposes
+custom_port = 49153 #ports from 49152-65535 can be used for custom purposes
 bad_port = True
 chatroom = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-hostname = socket.gethostbyaddr(socket.gethostname())[0]
+hostname = raw_input('what is the host? ')
 
 chatroom.connect((hostname, custom_port))
-chatroom.send("hello, world")
+chatroom.send("hello, luke")
 
