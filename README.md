73a5fb57b57248b773c4fc729e3cfca83fcf0144
Joshua Wyss <t96rocks@gmail.com>
2014-12-01 10:24:37 -0600

    Created Josh's Client

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is going to be AWESOME!
+
+>>Port Numbers
+The range 49152–65535 contains dynamic or private ports that cannot be registered with IANA.[169] This range is used for custom or temporary purposes and for automatic allocation of ephemeral ports.
--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import socket
 
 custom_port = 49152 #ports from 49152-65535 can be used for custom purposes
@@ -8,3 +9,20 @@ hostname = socket.gethostbyaddr(socket.gethostname())[0]
 chatroom.connect((hostname, custom_port))
 chatroom.send("hello, world")
 
+=======
+import sys
+import socket
+
+
+serverName = raw_input('What server do you want to connect to? ex. rc02xcs213.managed.mst.edu ')
+portNumber = raw_input('What port? ')
+userName = raw_input('What do you want your name to be? ex. Fred ')
+
+server_address = (str(serverName), int(portNumber))
+try:
+    socket.create_connection(server_address)
+else:
+    print('That did not work, try again. \n')
+    serverName = raw_input('What server do you want to connect to? ex. rc02xcs213.managed.mst.edu ')
+    portNumber = raw_input('What port? ')
+>>>>>>> Stashed changes
