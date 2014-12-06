edc034ccc86479c1f4f1458f880254aa160879e4
lbs5g8 <lbs5g8@mst.edu>
2014-12-05 18:55:55 -0600

    MINE

--- connection_tester.py
+++ connection_tester.py
@@ -1,10 +1,26 @@
 import socket
+import threading
 
-custom_port = 49152 #ports from 49152-65535 can be used for custom purposes
+custom_port = input("port: ") #ports from 49152-65535 can be used for custom purposes
 bad_port = True
 chatroom = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 hostname = socket.gethostbyaddr(socket.gethostname())[0]
 
 chatroom.connect((hostname, custom_port))
-chatroom.send("hello, world")
+
+def listen_for_messages(connection):
+	while(True):
+		print connection.recv(4096)
+
+user_name = raw_input("User name: ")
+chatroom.send(user_name)
+
+while True:
+	listen_thread = threading.Thread(target=listen_for_messages, args=(chatroom,))
+	listen_thread.start()
+
+	msg = raw_input("Msg: ")
+	chatroom.send(msg)
+
+
 
