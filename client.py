37dd20063a1123a62c0775419ba6276ff460f7fb
Trey Nickelsen <jtng28@mst.edu>
2015-01-31 11:23:23 -0600

    Updated for python 3.4

--- client.py
+++ client.py
@@ -18,35 +18,35 @@ def set_up_connection():
     bad_port = True
     while(bad_port):
         try:
-            serverName = raw_input('Server (i.e. rc02xcs213.managed.mst.edu): ')
+            serverName = input('Server (i.e. rc02xcs213.managed.mst.edu): ')
             
             try:
-                portNumber = input('Port: ')
+                portNumber = int(input('Port: '))
             except NameError:
-                print "Port must be a number."
+                print("Port must be a number.")
                 portNumber = -1
 
             sock = socket.create_connection((str(serverName), portNumber))
-        except socket.error , msg:
-            print 'Binding failed. Error code: ' + str(msg[0]) + ' Error message: ' + msg[1]
+        except socket.error as msg:
+            print('Binding failed. Error code: ' + str(msg[0]) + ' Error message: ' + msg[1])
         else:
             bad_port = False
-            print 'You successfully connected to ' + serverName + ' on port ' + str(portNumber) + '\n'
+            print('You successfully connected to ' + serverName + ' on port ' + str(portNumber) + '\n')
     return sock
 
 def get_user_name(conn):
     bad_user_name = True
     while(bad_user_name):
-        userName = raw_input('Choose a user name (i.e. Fred): ')
-        conn.send(userName)
-        valid_name = conn.recv(4096)
+        userName = input('Choose a user name (i.e. Fred): ')
+        conn.send(userName.encode('utf-8'))
+        valid_name = conn.recv(4096).decode('utf-8')
         if "True" in valid_name:
             bad_user_name = False
             total_users = valid_name.split(',')[1]
-            print 'Welcome' , userName + "!", "There are currently", total_users, "other users logged on.\n"
-            print "To send a message, simply type your message and hit enter."
+            print('Welcome' , userName + "!", "There are currently", total_users, "other users logged on.\n")
+            print("To send a message, simply type your message and hit enter.")
         elif "False" in valid_name:
-            print 'Server said that ' + userName + ' is already in use. Try another one.\n'
+            print('Server said that ' + userName + ' is already in use. Try another one.\n')
         elif "/too_many" in valid_name:
             sys.exit("Too many users currently connected. Try again later. Goodbye.")
 
@@ -55,26 +55,26 @@ def startClient(conn):
     while send_hold:
         try:
             # Message Sending
-            message = raw_input()
+            message = input()
             if message == '/exit' or message == '/quit' or message == '/part':
                 send_hold = False
             if message == '/connection_closed':
-                conn.send('/connection_closed/') #replace this so the user doesn't accidentally send an admin command
+                conn.send('/connection_closed/'.encode('utf-8')) #replace this so the user doesn't accidentally send an admin command
             elif len(server_disconnected) == 0:
-                conn.send(message)
+                conn.send(message.encode('utf-8'))
             elif len(server_disconnected) == 1:
                 sys.exit("Goodbye.")
         except KeyboardInterrupt:
             print('\n\n### Sorry, but to shut down please type one of these and press enter: /exit, /quit, or /part\n')
-        except socket.error, e:
-            print "WINNING"
+        except socket.error as e:
+            print("WINNING")
 
 def clientShutdown(conn):
     conn.close()
     sys.exit('\nYou have left the chatroom.\n')
 
 def close_connection(conn):
-    conn.send('/connection_closed')
+    conn.send('/connection_closed'.encode('utf-8'))
     conn.close()
     server_disconnected.append(True)
     print("Server now disconnected. Press any key to exit the program.")
@@ -84,7 +84,7 @@ def close_connection(conn):
 def readMessage(conn):
     read_hold = True
     while read_hold and len(server_disconnected) == 0:
-        recvMessage = conn.recv(4096)
+        recvMessage = conn.recv(4096).deocde('utf-8')
         if recvMessage == '/shutdown':
             close_socket_wait = threading.Timer(10, close_connection, (conn,))
             print("Server disconnecting. Connection will close after 10 seconds... ")
@@ -94,9 +94,9 @@ def readMessage(conn):
         elif recvMessage == '/bye': 
             read_hold = False
             conn.close()
-            print "Goodbye."
+            print("Goodbye.")
         else:
-            print recvMessage
+            print(recvMessage)
 
 sock = set_up_connection()
 get_user_name(sock)
--- server.py
+++ server.py
@@ -8,9 +8,11 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * '''
 import socket
 import threading
-import thread
+import _thread
 import sys
 import errno
+
+thread = _thread
 
 mutex = threading.Lock()
 
@@ -32,7 +34,7 @@ def start_server():
 				custom_port += 1 #increment the port number by 1
 			else:
 				bad_port = False
-				print "Using host", "'" + hostname + "'", "and port", custom_port
+				print("Using host", "'" + hostname + "'", "and port", custom_port)
 		
 		if custom_port == 65536:
 			sys.exit("Error: Unable to find a port that is not already taken. Something has gone wrong...")
@@ -40,41 +42,41 @@ def start_server():
 		try:
 			chatroom.listen(4) #begin listening for incoming requests.
 		except socket.error as e:
-			print e
+			print(e)
 			sys.exit("Chatroom unable to listen for connections. Ending program.")
 
 		while True: #Always accept new connections
 			try:
 				conn, address = chatroom.accept() #conn is the socket, address is a tuple of (host, port)
 			except socket.error as e:
-				print e
+				print(e)
 				sys.exit("Unable to accept connection. Ending program")
 
 			if len(clients) < 10:
 				user_name_exists = True
 				while(user_name_exists):
 					try:
-						chosen_user_name = conn.recv(4096) #the first message from the client is the name
+						chosen_user_name = conn.recv(4096).decode('utf-8') #the first message from the client is the name
 					except socket.error as e:
 						if e.errno == errno.ECONNRESET:
-							print "Client quit before entering a user name"
+							print("Client quit before entering a user name")
 							break
 					
 					if chosen_user_name not in user_names_set:
 						user_name_exists = False
 						
-						conn.send("True," + str(len(clients))) #Communicate to the client that the name is not taken
+						conn.send(("True," + str(len(clients))).encode('utf-8')) #Communicate to the client that the name is not taken
 						client = {}
 						client['user_name'] = chosen_user_name
 						client['connection'] = conn
 						
-						print chosen_user_name + " has joined the chatroom."
+						print(chosen_user_name + " has joined the chatroom.")
 						
 						mutex.acquire()
 						clients.append(client)
 						for client in clients:
 							if client['user_name'] != chosen_user_name:
-								client['connection'].send(chosen_user_name + " has joined the chatroom.\n")
+								client['connection'].send((chosen_user_name + " has joined the chatroom.\n").encode('utf-8'))
 						user_names_set.add(chosen_user_name)#add the unique name to the list
 						mutex.release()
 						
@@ -82,17 +84,17 @@ def start_server():
 							msg_thread = threading.Thread(target=listen_for_msgs, args=(client,chatroom))
 							msg_thread.start() #Begin a new thread that listens for messages from this client
 						except thread.error as e:
-							print e
+							print(e)
 							sys.exit("Unable to start listening thread. Ending program.")
 
 					else:
-						conn.send("False") #Communicate to the client that the name is taken
-						print chosen_user_name, "already taken."
+						conn.send("False".encode('utf-8')) #Communicate to the client that the name is taken
+						print(chosen_user_name, "already taken.")
 			else:
-				print "Too many clients are attempting to connect"
-				conn.send("/too_many")
+				print("Too many clients are attempting to connect")
+				conn.send("/too_many".encode('utf-8'))
 	except KeyboardInterrupt:
-		print "\nServer has initiated shutdown"
+		print("\nServer has initiated shutdown")
 		warn_and_close(chatroom)
 
 def listen_for_msgs(connection, chatroom):
@@ -101,8 +103,8 @@ def listen_for_msgs(connection, chatroom):
 
 	while(user_has_left == False and server_closing == False):
 		try:
-			msg = connection['connection'].recv(4096)
-			print msg
+			msg = connection['connection'].recv(4096).decode('utf-8')
+			print(msg)
 			if msg == '/connection_closed': #A client will send /connection_closed to acknowledge the server shutdown.s
 				mutex.acquire()
 				clients.remove(connection)
@@ -111,7 +113,7 @@ def listen_for_msgs(connection, chatroom):
 				
 				if len(clients) == 0: #If all clients have acknowledged the shutdown and themselves closed, the chatroom can close
 					chatroom.close()
-					print "Goodbye."
+					print("Goodbye.")
 					sys.exit()
 				
 				server_closing = True
@@ -121,7 +123,7 @@ def listen_for_msgs(connection, chatroom):
 				clients.remove(connection)
 				user_names_set.remove(connection['user_name'])
 				mutex.release()
-				connection['connection'].send("/bye") #Tell the client that it is no longer listening to it. The client will disconnect itself
+				connection['connection'].send("/bye".encode('utf-8')) #Tell the client that it is no longer listening to it. The client will disconnect itself
 				user_has_left = True
 			
 			if msg == '/connection_closed/': #used so the user doesn't inadvertantly send an admin command
@@ -133,27 +135,27 @@ def listen_for_msgs(connection, chatroom):
 				msg = connection['user_name'] + " has left the chatroom."
 
 			if (server_closing == False):
-				print msg
+				print(msg)
 				mutex.acquire()
 				for client in clients:
 					if client['user_name'] != connection['user_name']:
-						client['connection'].send(msg)
+						client['connection'].send(msg.encode('utf-8'))
 				mutex.release()
 
 		except socket.error as e:
 			if e.errno == errno.ECONNRESET:
-				print e
+				print(e)
 				user_has_left = True
-				print "No longer listening to", connection['user_name']
+				print("No longer listening to", connection['user_name'])
 
 				mutex.acquire()
 				clients.remove(connection)
 				user_names_set.remove(connection['user_name'])
 				mutex.release()
 			elif e.errno == errno.EPIPE:
-				print e
+				print(e)
 				user_has_left = True
-				print "No longer listening to", connection['user_name']
+				print("No longer listening to", connection['user_name'])
 
 				mutex.acquire()
 				clients.remove(connection)
@@ -164,9 +166,9 @@ def listen_for_msgs(connection, chatroom):
 
 def warn_and_close(connection):
 	if len(clients) == 0:
-		print "No clients connected. Goodbye."
+		print("No clients connected. Goodbye.")
 	else:
 		for client in clients:
-			client['connection'].send("/shutdown")
+			client['connection'].send("/shutdown".encode('utf-8'))
 
 start_server()
